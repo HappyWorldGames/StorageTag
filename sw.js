93461788c8eb3e295e4f2c322def250489ff3e2d
HappyWorldGames <xeler.arsk@gmail.com>
2025-08-19 10:44:41 +0300

    test

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'storage-tag-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
